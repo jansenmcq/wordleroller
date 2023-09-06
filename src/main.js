6686430c133706d14420ccbd64fc7e061a09cd08
Jansen McQuivey <jmcquivey@aurora.tech>
2023-09-06 15:27:47 -0700

    updates

--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faChevronDown, faChevronUp);
 
+window.testObj = {
+    a: [1,2,3,4,5],
+    b: 'bobert',
+    c: { foo: 'bar' },
+}
+
+window.foo = () => {
+    alert('hello');
+}
+
 createApp(App)
     .component('fa-icon', FontAwesomeIcon)
     .mount('#app')
